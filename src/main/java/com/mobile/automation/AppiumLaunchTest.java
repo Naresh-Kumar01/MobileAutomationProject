ed32a069e78a0d387721dddec2cbbf249439fdab
Naresh-Kumar01 <Nareshsofttechh@gmail.com>
2026-06-12 23:38:31 -0700

    chore: converted main method to testng annotation for jenkins

--- src/main/java/com/mobile/automation/AppiumLaunchTest.java
+++ src/main/java/com/mobile/automation/AppiumLaunchTest.java
@@ -2,21 +2,22 @@ package com.mobile.automation;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.Test; // Naya import
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AppiumLaunchTest {
-    public static void main(String[] args) {
 
-        // 1. Emulator (Pixel 8) ke hisab se capabilities configure ki hain
+    @Test // Ab yeh pure Maven test ki tarah run hoga
+    public void launchSettingsTest() {
+
         UiAutomator2Options options = new UiAutomator2Options()
-                .setDeviceName("emulator-5554")              // Realme ID ki jagah ab Emulator ID aayegi
-                .setPlatformVersion("14")                    // Hamare naye Pixel 8 ka Android version
+                .setDeviceName("emulator-5554")              
+                .setPlatformVersion("14")                    
                 .setAppPackage("com.android.settings")
                 .setAppActivity("com.android.settings.Settings");
+        
         options.setCapability("appium:androidHome", "C:\\Users\\Admin\\AppData\\Local\\Android\\Sdk");
-                
-        // Stable Timeouts & Session Handlers
         options.setCapability("appium:androidInstallTimeout", 180000);
         options.setCapability("appium:uiautomator2ServerInstallTimeout", 180000);
         options.setCapability("appium:adbExecTimeout", 180000);
@@ -27,14 +28,12 @@ public class AppiumLaunchTest {
 
         try {
             System.out.println("🚀 Appium Server se connect ho raha hai (Target: Pixel 8 Emulator)...");
-            
-            // Appium 2.x server default local url
             URL serverUrl = new URL("http://127.0.0.1:4723/");
 
             driver = new AndroidDriver(serverUrl, options);
             System.out.println("🥳 Success! Pixel 8 Emulator me Settings app successfully khul gayi hai!");
             
-            Thread.sleep(4000); // 4 seconds tak open rakhein
+            Thread.sleep(4000); 
 
         } catch (MalformedURLException e) {
             System.out.println("❌ URL galat hai: " + e.getMessage());
